3d8f3fbafa22d03779191303ec13bbafc5477d10
asdementeva <topchiy.alexandra.com>
2022-08-02 01:51:17 +0300

    feat: done

--- src/index.js
+++ src/index.js
@@ -1,3 +1,11 @@
 module.exports = function reverse (n) {
-  
+    
+    if (n > 0){
+    let a = String(n).split('').reverse().join('');
+    return +a;
+    } 
+
+    let a = String(num).split('').slice(1).reverse().join('');
+    return +a;
+    
 }

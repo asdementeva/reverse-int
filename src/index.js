72f18c837b5160aa662af4b250ee6a53c9bc380f
asdementeva <topchiy.alexandra.com>
2022-08-02 13:01:15 +0300

    feat: correct

--- src/index.js
+++ src/index.js
@@ -1,11 +1,12 @@
 module.exports = function reverse (n) {
     
     if (n > 0){
-    let a = String(n).split('').reverse().join('');
-    return +a;
-    } 
-
-    let a = String(num).split('').slice(1).reverse().join('');
-    return +a;
+        let a = String(n).split('').reverse().join('');
+        return +a;
+        } else {
+    
+         a = String(n).split('').slice(1).reverse().join('');
+        return +a;
+        }
     
 }
